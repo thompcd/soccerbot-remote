48ef48f58738a880da85ec581eb7c084c9a4eb9b
Corey Thompson <corey.dwayne.thompson@gmail.com>
2023-07-17 12:11:19 +0000

    Add Id and constructor to MoveMessage

--- soccerbot-remote-server/Data/MoveMessage.cs
+++ soccerbot-remote-server/Data/MoveMessage.cs
@@ -1,6 +1,15 @@
 [Serializable]
 public class MoveMessage
 {
+    public Guid Id {get;set;}
     public int Degrees {get;set;}
     public int Magnitude {get;set;}
+
+    #region Methods
+    public MoveMessage(int degrees, int magnitude){
+        Id = Guid.NewGuid();
+        Degrees = degrees;
+        Magnitude = magnitude;
+    }
+    #endregion
 }
